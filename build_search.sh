fa39ac1fe682d5a0ea47a059d5006bb9f7ddda23
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-30 21:07:32 +0530

    ci: add build search

--- build_search.sh
+++ build_search.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 wget https://github.com/jameslittle230/stork/releases/download/v1.6.0/stork-ubuntu-20-04
 chmod +x stork-ubuntu-20-04
-hugo --gc --minify -s exampleSite
+# hugo --gc --minify -s exampleSite $ prerequisite
 ./stork-ubuntu-20-04 --build exampleSite/public/index.toml
