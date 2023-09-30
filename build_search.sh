9987dbb369e7a78ae7c4dc9755b763080c0990de
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-09-30 21:30:48 +0530

    ci: update search index

--- build_search.sh
+++ build_search.sh
@@ -2,4 +2,4 @@
 wget https://github.com/jameslittle230/stork/releases/download/v1.6.0/stork-ubuntu-20-04
 chmod +x stork-ubuntu-20-04
 # hugo --gc --minify -s exampleSite $ prerequisite
-./stork-ubuntu-20-04 build exampleSite/public/index.toml
+./stork-ubuntu-20-04 build --input exampleSite/public/index.toml --output exampleSite/public/index.st
