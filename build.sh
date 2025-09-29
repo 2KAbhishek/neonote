14b3d98ce33f12f53e6b980f1ad3a56b9bb9e01b
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-09-29 07:50:16 +0530

    ci: bump stork to 22.04

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget https://github.com/jameslittle230/stork/releases/download/v1.6.0/stork-ubuntu-20-04
-chmod +x stork-ubuntu-20-04
+wget https://github.com/jameslittle230/stork/releases/download/v1.6.0/stork-ubuntu-22-04
+chmod +x stork-ubuntu-22-04
 hugo --gc --minify -s exampleSite
-./stork-ubuntu-20-04 build --input exampleSite/public/index.toml --output exampleSite/public/posts.st
+./stork-ubuntu-22-04 build --input exampleSite/public/index.toml --output exampleSite/public/posts.st
